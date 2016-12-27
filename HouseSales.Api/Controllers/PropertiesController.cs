92d49541b1e5152225b9738c47948fab6082b483
Coinio <coinio@users.noreply.github.com>
2016-12-27 14:21:32 +0000

    Small code tidy up

--- HouseSales.Api/Controllers/PropertiesController.cs
+++ HouseSales.Api/Controllers/PropertiesController.cs
@@ -2,6 +2,7 @@
 using HouseSales.Domain;
 using HouseSales.Repositories;
 using Postcodes.Repositories;
+using System;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.ModelBinding;
@@ -26,7 +27,7 @@ namespace HouseSales.Api.Controllers
             if (modelState.Count > 0)
                 return BadRequest(modelState);
 
-            var findCriteria = model.ToFindCriteria(500);
+            var findCriteria = GetFindCriteria(model, 500);
             var orderBy = model.OrderBy;
             var paging = new Paging(model.Page, 25);
 
@@ -45,6 +46,17 @@ namespace HouseSales.Api.Controllers
                 modelStateDictionary.AddModelError("Postcode", "Postcode does not exist.");
 
             return modelStateDictionary;
+        }
+
+        private PropertyFindCriteria GetFindCriteria(PropertiesRequestModel model, int resultLimit)
+        {
+            return new PropertyFindCriteria()
+            {
+                Postcode = model.Postcode,
+                PropertyType = model.PropertyType,
+                LastSaleDate = model.SearchRangeInMonths <= 0 ? default(DateTime) : DateTime.Now.AddMonths(-model.SearchRangeInMonths),
+                ResultLimit = resultLimit
+            };
         }
     }
 }
--- HouseSales.Api/Infrastructure/ModelBinders/PropertiesRequestModel.cs
+++ HouseSales.Api/Infrastructure/ModelBinders/PropertiesRequestModel.cs
@@ -32,42 +32,6 @@ namespace HouseSales.Api.Infrastructure
         /// </summary>
         public OrderBy OrderBy { get; set; }
 
-        /// <summary>
-        /// Convert to the values to a PropertyFindCriteria object
-        /// </summary>
-        /// <param name="resultLimit">Limit the maximum number of results returned from the search.</param>
-        /// <returns></returns>
-        public PropertyFindCriteria ToFindCriteria(int resultLimit = 0)
-        {
-            return new PropertyFindCriteria()
-            {
-                Postcode = this.Postcode,
-                PropertyType = this.PropertyType,
-                LastSaleDate = this.SearchRangeInMonths <= 0 ? default(DateTime) : DateTime.Now.AddMonths(-this.SearchRangeInMonths),
-                ResultLimit = resultLimit
-            };
-        }
-
-        /// <summary>
-        /// Quick and dirty validation method for the postcode.
-        /// Needs converting into a validation attribute.
-        /// </summary>
-        public bool IsPostcodeFormatValid
-        {
-            get
-            {
-
-                if (String.IsNullOrEmpty(Postcode))
-                    return false;
-                if (Postcode.Length < 3 || Postcode.Length > 8)
-                    return false;
-                if (Postcode.Length > 4 && !Postcode.Contains(" "))
-                    return false;
-
-                return true;
-            }
-        }
-
         private void SetDefaultValues()
         {
             Postcode = String.Empty;

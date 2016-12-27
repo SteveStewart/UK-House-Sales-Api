b76ea6e6337d9d3f5318d4853922db16171ff83d
Coinio <coinio@users.noreply.github.com>
2016-12-27 12:55:12 +0000

    Rename Postcode project to Postcodes.Domain

---
+++ Postcodes.Domain/Domain/Postcode.cs
@@ -0,0 +1,20 @@
+﻿using Geolocation;
+using System;
+
+namespace Postcodes.Domain
+{
+    /// <summary>
+    /// A class to hold a postcode value
+    /// </summary>
+    public class Postcode
+    {
+        /// <summary>
+        /// The actual postcode value
+        /// </summary>
+        public String Value { get; set; }
+        /// <summary>
+        /// The approximate location of the postcode
+        /// </summary>
+        public DecimalGeoCoordinate Location { get; set; }
+    }
+}
